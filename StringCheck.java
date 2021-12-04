965bb273b33c9820d689f2d6a5bae45d90add35a
ShivamKumar8340 <shivam.kumar1111@outlook.com>
2021-12-04 13:00:18 +0530

    Commited String comparison program

---
+++ StringCheck.java
@@ -0,0 +1,35 @@
+class StringCheck  {
+	public static void main(String args[]) {
+		String s1="SHIVAM";
+		String s2 = new String("SHIVAM");
+		String s3= "SHIVAM";
+		String s4=new String("shivam");
+		
+		/*System.out.println(s1.equals(s2));  //equals
+		System.out.println(s1.equals(s3));
+		System.out.println(s1.equals(s4));
+		
+		System.out.println(s1==s2);				//==
+		System.out.println(s1==s3);
+		System.out.println(s1==s4);
+		
+		System.out.println(s1.compareTo(s2)); //0    //compareTo
+		System.out.println(s1.compareTo(s4)); //s1<s4 negative 
+		
+		//concat mathode
+		
+		s1=s1.concat("XYZ");
+								//SHIVAMXYZ
+		
+		System.out.println(s1);	
+		
+			s1=s1.concat("10"+"2");
+			System.out.println(s1);	*/
+			 String s7= 30+35+"shivam"+10+29;
+			System.out.println(s7);
+		
+		
+		
+	}
+}
+	
